2384708f523b01f78a0bf66fd7c777f3e12079c7
vickyID <vineet1.singh@citi.com>
2020-01-12 03:27:54 +0000

    seeder for bootcamp

---
+++ seeder.js
@@ -0,0 +1,50 @@
+const fs = require("fs");
+const colors = require("colors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+//load configfile
+dotenv.config({ path: "./config/config.env" });
+
+// Load Models
+const Bootcamp = require("./models/Bootcamp");
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+
+//Read JSON
+
+const bootcamps = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
+);
+
+// import into db
+const importData = async () => {
+  try {
+    await Bootcamp.create(bootcamps);
+    console.log("Data imported....".green.inverse);
+    process.exit();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+    console.log("Data destroyed....".red.inverse);
+    process.exit();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+if (process.argv[2] === "-i") {
+  importData();
+} else if (process.argv[2] === "-d") {
+  deleteData();
+}
