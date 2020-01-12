9568850594b5511e2971ea4da9f5a89da4b800da
vickyID <vineet1.singh@citi.com>
2020-01-12 02:43:02 +0000

    slug pre save hook added to Bootcamp Schema

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -92,6 +93,13 @@ const BootcampSchema = new mongoose.Schema({
     type: Date,
     default: Date.now
   }
+});
+
+//Create bootcamp slug from the name
+BootcampSchema.pre("save", function(next) {
+  // console.log("Slugify ran", this.name);
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 module.exports = mongoose.model("Bootcamp", BootcampSchema);
