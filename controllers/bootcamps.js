2588df854854c9fb59f6752a18ed85fbad972341
vickyID <vineet1.singh@citi.com>
2020-01-12 14:29:17 +0000

    pagination for Bootcamp resource

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -16,7 +16,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 
   // Fields to exclude
   // Loop over removeFields and delete then from reqQuery
-  const removeFields = ["select", "sort"];
+  const removeFields = ["select", "sort", "page", "limit"];
   removeFields.forEach(param => delete reqQuery[param]);
   console.log(reqQuery);
 
@@ -46,12 +46,33 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     query = query.sort("-createdAt");
   }
 
+  //Pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 25;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Bootcamp.countDocuments();
+
+  query = query.skip(startIndex).limit(limit);
+
   // Executing query
   const bootcamps = await query;
+
+  // Pagination result
+  const pagination = {};
+
+  if (endIndex < total) {
+    pagination.next = { page: page + 1, limit: limit };
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = { page: page - 1, limit: limit };
+  }
 
   res.status(200).json({
     success: true,
     count: bootcamps.length,
+    pagination: pagination,
     data: bootcamps
   });
 });
