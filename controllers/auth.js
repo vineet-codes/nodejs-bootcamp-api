ae04c111e7d5986dbccc616c57954a2126d25d22
vickyID <vineet1.singh@citi.com>
2020-01-19 18:33:19 +0000

    update user details and password

--- controllers/auth.js
+++ controllers/auth.js
@@ -53,7 +53,41 @@ exports.login = asyncHandler(async (req, res, next) => {
 // @route   GET /api/v1/auth/me
 // @access  private
 exports.getMe = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.user.id);
+  const user = await await User.findById(req.user.id);
+
+  res.status(200).json({ success: true, data: user });
+});
+
+// @desc    Update logged in user's password
+// @route   PUT /api/v1/auth/updatepassword
+// @access  private
+exports.updatePassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select("+password");
+
+  // check current passwors
+  if (!(await user.matchPassword(req.body.currentPassword))) {
+    return next(new ErrorResponse(`Password is incorrect`, 401));
+  }
+
+  user.password = req.body.newPassword;
+  await user.save();
+
+  sendTokeResponse(user, 200, res);
+});
+
+// @desc    Update user details
+// @route   PUT /api/v1/auth/updatedetails
+// @access  private
+exports.updateDetails = asyncHandler(async (req, res, next) => {
+  const fieldsToUpdate = {
+    name: req.body.name,
+    email: req.body.email
+  };
+
+  const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+    new: true,
+    runValidators: true
+  });
 
   res.status(200).json({ success: true, data: user });
 });
